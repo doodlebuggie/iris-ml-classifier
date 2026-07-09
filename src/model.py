37589b44e313c2a630d27a058d7482f20987130e
doodlebuggie <whisgarydhaakirah@gmail.com>
2026-07-09 12:59:41 +0200

    train knn classifier and evaluate accuracy

--- src/model.py
+++ src/model.py
@@ -4,22 +4,30 @@ This module us used to build the classfier.
 """
 
 import pandas as pd
+import numpy as np
 import sklearn
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 data = load_iris()
 X = data.data
 y = data.target
 #shape = data.data.shape
 
-df = pd.DataFrame(data.data, columns=data.feature_names)
+df = pd.DataFrame(X, columns=data.feature_names)
 df['species'] = y
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,shuffle=True, random_state=42)
 
-print(X_train.shape)
-print(X_test.shape)
-print(y_train.shape)
-print(y_test.shape)
+knn = KNeighborsClassifier(n_neighbors=3) # create the model
+knn.fit(X_train, y_train) # train model using only taining data
+y_pred = knn.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print(accuracy)
+
+
+
