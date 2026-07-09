3cba14f24e9ddec23174f0a77be61b48a7e2e548
doodlebuggie <whisgarydhaakirah@gmail.com>
2026-07-09 18:14:58 +0200

    Add Streamlit UI for Iris flower Prediction

--- src/app.py
+++ src/app.py
@@ -3,6 +3,28 @@
 # display prediction
 
 import streamlit as st
-import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
 
-st.write(""" # my first app Hello *world!* """)
+st.markdown("# :violet[Iris flower Classifier]")
+
+data = load_iris()
+X = data.data
+y = data.target
+
+knn = KNeighborsClassifier(n_neighbors=3) # create the model
+
+sepal_length = st.number_input("Enter the sepal length")
+sepal_width = st.number_input("Enter the sepal width")
+petal_length = st.number_input("Enter the petal length")
+petal_width = st.number_input("Enter the petal width")
+
+predict_button = st.button("Predict")
+knn.fit(X, y) # train model using only taining data
+
+if predict_button: 
+    iris = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = knn.predict(iris)
+    species = data.target_names[prediction[0]]
+    st.success(f"Prediction: {species}")
